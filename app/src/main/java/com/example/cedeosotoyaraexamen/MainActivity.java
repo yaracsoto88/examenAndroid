caee05255864b02f835929c45129fdbf1929b0b4
yaracsoto88 <_cedenosoto@cifpfbmoll.eu>
2023-11-30 20:22:09 +0100

    bien

--- app/src/main/java/com/example/cedeosotoyaraexamen/MainActivity.java
+++ app/src/main/java/com/example/cedeosotoyaraexamen/MainActivity.java
@@ -6,12 +6,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame1, new fr1())
+                    .addToBackStack(null)
+                    .commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame1, new fr1())
+                    .addToBackStack(null)
+                    .commit();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame2, new fr2())
+                    .addToBackStack(null)
+                    .commit();
+        }
 
     }
 }
-
